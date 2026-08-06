6c176945ed5233933500fac25b34af19723706b5
spajicdominik <dominikspajic7@gmail.com>
2026-08-06 09:49:43 +0200

    Create TournamentController.java

---
+++ tcrs-app/src/main/java/com/tcrs_app/tcrs_app/controllers/TournamentController.java
@@ -0,0 +1,11 @@
+package com.tcrs_app.tcrs_app.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/tournaments")
+@RequiredArgsConstructor
+public class TournamentController {
+}
